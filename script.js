33812a9131e20d90a8e81f768aa0e1ae4668aedd
LyeTilley <74318846+LyeTilley@users.noreply.github.com>
2022-05-29 18:43:34 -0300

    second

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-$(function () { //document ready
+// lyle pizza
 
-	// Pizza Data Object
+$(function () { 
+
 	const pizza = {
 		pie: [
 			{
@@ -36,31 +37,29 @@ $(function () { //document ready
 		]
 	};
 
-	// Random Result Function to randomize the results
 	const randomResult = arr => {
 		const randomNumber = Math.floor(Math.random() * arr.length);
 		return arr[randomNumber];
 	};
 
-	//Adding an Event Listener for when the form is submitted
+	
 	$('form').on('submit', function (event) {
 		event.preventDefault();
-		//These cannot be cached, because they may change due to the advanced selector of :checked
+	
 		const userSizeSelection = $("input[name=size]:checked").val();
 		const userPriceSelection = $('input[name=price]:checked').val();
 
-		//Selecting the specific array inside of the Pizza Data Object that reflects user choice of size
+	
 		const sizeOptions = pizza[userSizeSelection];
 
-		//Filtering the options in that size for the user choice of price
+		
 		const filteredOptions = sizeOptions.filter((restaurant) => {
 			return (restaurant.price === userPriceSelection);
 		});
 
-		//Randomly selection of one of the options that match users choices
 		const theOne = randomResult(filteredOptions);
 
-		//Adding the result to the page
+	
 		const finalHTML = `<h2>You should go to <span class="restaurant">${theOne.title}</span></h2>`;
 		$('.results').html(finalHTML);
 	});
